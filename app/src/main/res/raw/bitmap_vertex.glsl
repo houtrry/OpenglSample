3ef2f4468cf6c0bb49dbde83c493a1dcda5db4a3
huangdejun <huangdejun@pudutech.com>
2024-06-11 00:08:11 +0800

    add code source

---
+++ app/src/main/res/raw/bitmap_vertex.glsl
@@ -0,0 +1,10 @@
+attribute vec4 inputTextureCoordinate;
+
+varying vec2 textureCoordinate;
+
+attribute vec4 vPosition;
+
+void main() {
+    gl_Position = vPosition;
+    textureCoordinate = inputTextureCoordinate.xy;
+}
