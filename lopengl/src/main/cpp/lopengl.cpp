d8154b7c887f2e4a3498a29d26ae007602ceaa49
houtrry <houtrry@hotmail.com>
2024-06-16 14:05:50 +0800

    抽取公共方法

--- lopengl/src/main/cpp/lopengl.cpp
+++ lopengl/src/main/cpp/lopengl.cpp
@@ -5,51 +5,17 @@
 #include "landroidlog.h"
 #include "LOpenglPrimitivesDef.h"
 #include "Triangle.h"
-#include "LImage.h"
 #include "utils.h"
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/mat4x4.hpp>
-#include <glm/glm.hpp>
-#include <glm/ext/matrix_transform.hpp>
-#include <glm/ext/matrix_clip_space.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <android/asset_manager_jni.h>
-#include <android/asset_manager.h>
-
+#include "opengl_utils.h"
 
 #ifdef __cplusplus
 
 extern "C" {
 #endif
 float m_angle = 0.0f;
-/**
- * 创建纹理ID
- * @return
- */
-GLuint createTexture(LImage *image) {
-    //声明纹理id
-    GLuint textureId;
-
-    glEnable(GL_TEXTURE_2D);
-    //生成纹理id
-    glGenTextures(1, &textureId);
-    //绑定纹理id，之后的操作都针对当前的纹理索引
-    glBindTexture(GL_TEXTURE_2D, textureId);
-    //声明
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    //声明环绕方向
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-    //指定参数， 生成纹理（向GPU提交纹理数据）
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
-                 image->getWidth(), image->getHeight(),
-                 0, GL_RGBA, GL_UNSIGNED_BYTE,
-                 image->getImageData());
-    return textureId;
-}
 GLuint mTextureIds[6];
 
 /**
@@ -416,46 +382,6 @@ void Java_com_houtrry_lopengl_view_MapView_ndkDraw(JNIEnv *env, jobject thiz) {
 //    drawRotateCube();
 //    drawTexture();
     drawRotateTextureCube();
-}
-
-GLuint readSourceFromAssertManager(AAssetManager *mAssetManager, const char *fileName) {
-    if (NULL == mAssetManager) {
-        LOGF("mAssetManager is NULL");
-        return -1;
-    }
-    if (NULL == fileName) {
-        LOGF("fileName is NULL");
-        return -1;
-    }
-    LOGD ("FileName is %s", fileName);
-    AAsset *asset = AAssetManager_open(mAssetManager, fileName, AASSET_MODE_UNKNOWN);
-    if (NULL == asset) {
-        LOGF("asset is NULL");
-        return -1;
-    }
-    off_t bufferSize = AAsset_getLength(asset);
-    LOGD("  buffer size is %ld", bufferSize);
-
-    U8_t *imgBuff = (U8_t *) malloc(bufferSize + 1);
-    if (NULL == imgBuff) {
-        LOGF("imgBuff alloc failed");
-        return -1;
-    }
-    memset(imgBuff, 0, bufferSize + 1);
-    int readLen = AAsset_read(asset, imgBuff, bufferSize);
-    LOGD("  Picture read: %d", readLen);
-
-    LImage *glImage = LImage::readFromBuffer(imgBuff, readLen);
-    LOGD("  image: %d, %d", glImage->getWidth(), glImage->getHeight());
-    GLuint textureId = createTexture(glImage);
-    delete glImage;
-
-    if (imgBuff) {
-        free(imgBuff);
-        imgBuff = NULL;
-    }
-    AAsset_close(asset);
-    return textureId;
 }
 
 jint Java_com_houtrry_lopengl_view_MapView_ndkReadAssertManager(JNIEnv *env, jobject thiz,
