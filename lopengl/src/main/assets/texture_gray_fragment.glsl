776d7dd88ada8431a05199f9c99269231be1cc16
houtrry <houtrry@hotmail.com>
2024-10-21 00:36:40 +0800

    1. 优化代码

--- lopengl/src/main/assets/texture_gray_fragment.glsl
+++ lopengl/src/main/assets/texture_gray_fragment.glsl
@@ -9,10 +9,14 @@ void main() {
 //    outColor = texture(s_TextureMap, v_texCoord);
 //    flat gray;
 //    gray  = float(texture(s_TextureMap, v_texCoord).r);
-    vec3 rgb;
-    rgb.r = texture(s_TextureMap, v_texCoord).r;
-    rgb.g = rgb.r;
-    rgb.b = rgb.r;
-    outColor = vec4(rgb, 1.0);
+    float gray = texture(s_TextureMap, v_texCoord).r;
+    if(132.0 / 255.0 == gray) {
+        outColor = vec4(1.0, 1.0, 1.0, 1.0);
+    } else if (1.0 == gray) {
+        outColor = vec4(0.49725, 0.847059, 0.917647, 1.0);
+    } else {
+        outColor = vec4(0.0, 0.447, 1.0, 1.0);
+    }
+
 //    outColor = vec4(gray, gray, gray, 1.0);
 }
